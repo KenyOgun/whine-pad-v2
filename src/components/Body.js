c151c7cb83f853a3e169deff69054d32b9639d97
KenyOgun <holleraturboi@yahoo.ca>
2022-10-09 03:57:51 -0400

    initial commit

---
+++ src/components/Body.js
@@ -0,0 +1,7 @@
+import "./Body.css";
+import React from "react";
+
+const Body = ({ children }) => {
+  return <div className="Body">{children}</div>;
+};
+export default Body;
---
+++ src/components/Logo.js
@@ -0,0 +1,8 @@
+import logo from "./../images/whinepad-logo.svg";
+import React from "react";
+
+const Logo = () => {
+  return <img src={logo} width="300" alt="Whinepad logo" />;
+};
+
+export default Logo;
